04d460fa163ebf2512aeda0ae35f088e018ee7f2
Kevin Martello <kevinmartello@icloud.com>
2022-02-27 13:33:55 -0500

    Change url deploy

--- src/Functions/DbService.js
+++ src/Functions/DbService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { encrypt } from "./Encript"
-// const url = "https://quiet-mesa-11047.herokuapp.com/Users";
-const url = "http://localhost:8080/Users";
+const url = "https://quiet-mesa-11047.herokuapp.com/Users";
+// const url = "http://localhost:8080/Users";
 
 export const update = (id, param) => {
     return axios.patch(`${url}/${id}`, param);
